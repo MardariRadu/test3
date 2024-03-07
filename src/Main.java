8e045a67e3918c7ee1cbbd6607eb39c6d6b74a92
Radu mardari <radumardari89@gmail.com>
2024-03-07 05:56:24 +0300

    branch1

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("bug fix");
         System.out.println("master");
+        System.out.println("branch!1");
 
     }
 }
