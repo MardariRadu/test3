e3763b6d081e59a4933ed546ecf3f54aa8fe8a1d
Radu mardari <radumardari89@gmail.com>
2024-03-07 05:59:16 +0300

    branch22

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("bug fix");
+        System.out.println("B.U.G Mafia");
         System.out.println("master");
         System.out.println("Branch!22");
 
